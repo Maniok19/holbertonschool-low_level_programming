ccbe1dfabbcb42f1c4c780701f575916159665b9
Mano <mano19029@hotmail.fr>
2024-10-21 11:43:08 +0200

    First script in c

---
+++ hello_world/4-putss.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Print sentence
+ * Description: print the sentence
+ */
+
+int main(void)
+	{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); /* 0 for no error */
+	}
