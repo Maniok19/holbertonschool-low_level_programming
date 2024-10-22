4f86481d2101d67bb68e427a7e4f77029b59c1cc
Mano <mano19029@hotmail.fr>
2024-10-22 14:06:23 +0200

    Refactor main function in 0-putchar.c to include newline character

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - print _putchar
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -14,5 +14,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
