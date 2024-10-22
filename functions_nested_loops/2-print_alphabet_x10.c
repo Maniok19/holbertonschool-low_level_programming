8c637e9f76f4efe57c29243417f1ec08c3767927
Mano <mano19029@hotmail.fr>
2024-10-22 16:15:42 +0200

    Refactor print_alphabet_x10 function to fix variable declaration issue and add function to print alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,20 @@
 
 /**
  * print_alphabet_x10 - print alphabet 10x
+ * void print_alphabet - print alphabet 1x
  * Return: Always 0
  */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
 
 void print_alphabet_x10(void)
 {
