011eca69b69acab3ca95c5cafe241700b77885de
Mano <mano19029@hotmail.fr>
2024-10-22 16:11:00 +0200

    Add function to print alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10x
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (int i = 0; i < 10; i++)
+	{
+	print_alphabet();
+	}
+}
