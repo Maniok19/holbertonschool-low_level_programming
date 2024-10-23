af87875d1b9e1614128abf358ddabe139b2a5a92
Mano <mano19029@hotmail.fr>
2024-10-23 13:12:52 +0200

    Fix formatting in times_table function

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,18 +15,25 @@ void times_table(void)
 		for (y = 0; y <= 9; y++)
 		{
 			z = x * y;
-			if (z < 10)
+			if (y != 0)
 			{
-				_putchar(z + '0');
+				_putchar(',');
 				_putchar(' ');
 			}
-			else
+			if (z < 10 && y != 0)
+			{
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else if (z >= 10)
 			{
 				_putchar((z / 10) + '0');
 				_putchar((z % 10) + '0');
 			}
-			_putchar(',');
-			_putchar(' ');
+			else
+			{
+				_putchar(z = '0');
+			}
 		}
 		_putchar('\n');
 	}
