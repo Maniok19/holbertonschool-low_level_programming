45929eb28521789634055d98f6806afe1c607698
Mano <mano19029@hotmail.fr>
2024-10-23 09:30:29 +0200

    Add _abs function to calculate the absolute value of a number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ * @n: the integer to check
+ * Return: always 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (n);
+}
