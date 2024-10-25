efd49f1c941efb79d98c3056b49826fc23d379ac
Mano <mano19029@hotmail.fr>
2024-10-25 13:00:45 +0200

    Refactor loop condition to print Fibonacci sequence up to 50 terms

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -6,16 +6,19 @@
 int main(void)
 {
 	int count;
-	unsigned long int fib1 = 0;
-	unsigned long int fib2 = 1;
+	unsigned long int fib1 = 1;
+	unsigned long int fib2 = 2;
 	unsigned long int fibn;
 
-	for (count = 0; count < 50; count++)
+	printf("%lu, %lu", fib1, fib2);
+
+	for (count = 3; count < 50; count++)
 	{
 		fibn = fib1 + fib2;
-		printf("%lu\n", fibn);
+		printf(", %lu", fibn);
 		fib1 = fib2;
 		fib2 = fibn;
 	}
+	printf("\n");
 	return (0);
 }
