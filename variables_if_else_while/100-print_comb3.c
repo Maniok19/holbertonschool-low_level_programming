2c3c361b82da993d9e442084237494b1f14ff1c6
Mano <mano19029@hotmail.fr>
2024-10-25 09:35:44 +0200

    Refactor code to improve readability and maintainability

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,27 @@
 #include <unistd.h>
 #include <stdio.h>
 
+/**
+ * main - i love spaghetti
+ * Return: With bolognaise
+ */
 int main(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 9; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            putchar(i + '0');
-            putchar(j + '0');
-            if (i != 8 || j != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
