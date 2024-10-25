30120afc0e841a03d91451a5564e6de4a3d97bab
Mano <mano19029@hotmail.fr>
2024-10-25 13:32:40 +0200

    Refactor loop condition to print Fibonacci sequence up to 98 terms

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - i love u
+ * Return: Always 0
+ */
+int main(void)
+{
+	int count;
+	unsigned long int fib1 = 1;
+	unsigned long int fib2 = 2;
+	unsigned long int fibn;
+
+	printf("%lu, %lu", fib1, fib2);
+
+	for (count = 3; count <= 98; count++)
+	{
+		fibn = fib1 + fib2;
+		printf(", %lu", fibn);
+		fib1 = fib2;
+		fib2 = fibn;
+	}
+	printf("\n");
+	return (0);
+}
