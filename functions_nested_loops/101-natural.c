e791bea3d0feeaf0b7aa71fa412643c702154cff
Mano <mano19029@hotmail.fr>
2024-10-25 12:27:18 +0200

    Refactor loop condition to sum multiples of 3 and 5 up to 1024

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -7,13 +7,14 @@
 int main(void)
 {
 	int x;
-
+	int y;
 	for (x = 1; x < 1024; x++)
 	{
 		if (x % 3 == 0 || x % 5 == 0)
 		{
-			printf("%d\n", x);
+			y = x + y;
 		}
 	}
+	printf("%d", y);
 	return (0);
 }
