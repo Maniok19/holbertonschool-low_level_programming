1f93c5abd1cf7d2dbc0588536450fd703cb4b6d5
Mano <mano19029@hotmail.fr>
2024-10-25 13:12:21 +0200

    Refactor loop condition to sum even Fibonacci numbers up to 4 million

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - i love u
+ * Return: Always 0
+ */
+int main(void)
+{
+	int count;
+	unsigned long int fib1 = 0;
+	unsigned long int fib2 = 1;
+	unsigned long int fibn;
+	unsigned long int result;
+
+
+	for (count = 1; count <= 50; count++)
+	{
+		fibn = fib1 + fib2;
+		
+		if (fibn % 2 == 0 && fibn < 4000000)
+		{
+			result = result + fibn;
+		}
+		fib1 = fib2;
+		fib2 = fibn;
+	}
+	printf("%lu\n", result);
+	return (0);
+}
