cfd5b8c92346c0de5acd27d3275715510e0ad3e7
Mano <mano19029@hotmail.fr>
2024-10-28 13:57:38 +0100

    Add FizzBuzz program to print numbers from 1 to 100, replacing multiples of 3 with "Fizz", multiples of 5 with "Buzz", and multiples of both with "FizzBuzz"

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -FizzBuzz
+ * Return: always 0
+ */
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if (count % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		if (count % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		if (count % 3 != 0 && count % 5 != 0)
+		{
+			printf("%d", count);
+		}
+		printf(" ");
+	}
+}
