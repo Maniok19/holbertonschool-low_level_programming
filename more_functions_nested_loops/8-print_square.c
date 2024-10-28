7b357f3a5d833be7406e9b5b0a0b344a191b2469
Mano <mano19029@hotmail.fr>
2024-10-28 13:41:41 +0100

    Add print_square function to print a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - print a square
+ * @size: size bruh
+ */
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
