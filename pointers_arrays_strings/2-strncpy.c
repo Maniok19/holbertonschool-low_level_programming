4cd4a1e2c3e81924766e2e9de6d3c89ff2203532
Mano <mano19029@hotmail.fr>
2024-10-30 13:13:35 +0100

    Add _strncpy function to copy a string with a specified length

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: number to copy
+ * Return: destination
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int num = 0;
+
+	while (num < n && src[num] != '\0')
+	{
+		dest[num] = src[num];
+		num++;
+	}
+	while (num < n)
+	{
+	dest[num] = '\0';
+	num++;
+	}
+	return (dest);
+}
