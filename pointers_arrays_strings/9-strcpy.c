35601f5740f72fbf8cd98ac3c6fe817539d0d9aa
Mano <mano19029@hotmail.fr>
2024-10-30 10:26:21 +0100

    Refactor _strcpy function to fix typo and add return statement

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,6 +4,7 @@
  * _strcpy - copy the string
  * @dest: destination
  * @src: source
+ * Return: destination value
  */
 char *_strcpy(char *dest, char *src)
 {
@@ -11,9 +12,9 @@ char *_strcpy(char *dest, char *src)
 
 	while (src[num] != '\0')
 	{
-		dest[num] = src [num];
+		dest[num] = src[num];
 		num++;
 	}
 	dest[num] = '\0';
-	return dest;
+	return (dest);
 }
