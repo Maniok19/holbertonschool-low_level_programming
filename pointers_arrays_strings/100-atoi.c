b2f366edf4e7f13930d8c2545c6f01f0f1760cbb
Mano <mano19029@hotmail.fr>
2024-10-30 14:23:25 +0100

    Refactor _atoi function to handle edge cases and improve readability

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _atoi - convert string to integer
  * @s: source string
@@ -7,9 +8,7 @@ int _atoi(char *s)
 {
     int result = 0;
     int sign = 1;
-    int digit;
-    int max_div_10 = 214748364; // INT_MAX / 10
-    int max_last_digit = 7;     // INT_MAX % 10
+    int started = 0;
 
     while (*s)
     {
@@ -17,44 +16,22 @@ int _atoi(char *s)
             sign *= -1;
         else if (*s >= '0' && *s <= '9')
         {
-            digit = *s - '0';
-            
-            if (result > max_div_10 || (result == max_div_10 && digit > max_last_digit))
+            started = 1;
+            if (result > 214748364)
+                return (sign == 1) ? 2147483647 : -2147483648;
+            if (result == 214748364)
             {
-                return (sign == 1) ? 2147483647 : -2147483648; // INT_MAX or INT_MIN
+                if (sign == 1 && *s > '7')
+                    return 2147483647;
+                if (sign == -1 && *s > '8')
+                    return -2147483648;
             }
-            
-            result = result * 10 + digit;
+            result = result * 10 + (*s - '0');
         }
-        else if (result != 0)
+        else if (started)
             break;
         s++;
     }
 
-    if (sign == -1 && result == 2147483648) // Special case for -2147483648
-        return -2147483648;
-
     return result * sign;
-}
-
-int main(void)
-{
-    int nb;
-
-    nb = _atoi("98");
-    printf("%d\n", nb);
-    nb = _atoi("-402");
-    printf("%d\n", nb);
-    nb = _atoi("          ------++++++-----+++++--98");
-    printf("%d\n", nb);
-    nb = _atoi("214748364");
-    printf("%d\n", nb);
-    nb = _atoi("0");
-    printf("%d\n", nb);
-    nb = _atoi("Suite 402");
-    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-    printf("%d\n", nb);
-    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-    printf("%d\n", nb);
-    return (0);
 }
