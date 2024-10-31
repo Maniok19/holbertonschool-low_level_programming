eefcce59dd6d9afef2093b88fa524a72e91d5e12
Mano <mano19029@hotmail.fr>
2024-10-31 15:18:21 +0100

    Refactor leet function to use arrays for mapping characters to leet numbers

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -2,25 +2,25 @@
 #include <stdio.h>
 /**
  * leet - encode in 1337
- * @str: string to convert
+ * @s: string to convert
  * Return: destination
  */
 char *leet(char *s)
 {
-    int i, j;
-    char leet_letters[] = "aAeEoOtTlL";
-    char leet_numbers[] = "4433007711";
+	int i, j;
+	char leet_letters[] = "aAeEoOtTlL";
+	char leet_numbers[] = "4433007711";
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
-            if (s[i] == leet_letters[j])
-            {
-                s[i] = leet_numbers[j];
-                break;
-            }
-        }
-    }
-    return (s);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == leet_letters[j])
+			{
+				s[i] = leet_numbers[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
