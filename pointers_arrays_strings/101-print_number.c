05ed45b0683727bf62daf648f79919e743878f57
Mano <mano19029@hotmail.fr>
2024-11-03 16:32:47 +0100

    Refactor print_number function to handle printing of negative integers with up to 4 digits

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_number - print int
+ * @n: int to print
+ */
+void print_number(int n)
+{
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		_putchar('1');
+		_putchar('4');
+		_putchar('7');
+		_putchar('4');
+		_putchar('8');
+		_putchar('3');
+		_putchar('6');
+		_putchar('4');
+		_putchar('8');
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,5 +20,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *leet(char *str);
 char *rot13(char *str);
+void print_number(int n);
 
 #endif
