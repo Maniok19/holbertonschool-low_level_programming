ef81a944f8e3328ce0e4de4f7cf121700a1beb1a
Mano <mano19029@hotmail.fr>
2024-11-04 21:25:07 +0100

    project on pointed pointers

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locate character in a string
+ * @s: string source
+ * @c: char to find
+ * Return: pointer to the first occurence
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
