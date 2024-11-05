148d5efd315df099cda24ccfa4106ffda88b76af
Mano <mano19029@hotmail.fr>
2024-11-05 16:27:57 +0100

    project on recursion

--- recursion/2-main.c
+++ recursion/2-main.c
@@ -8,11 +8,9 @@
  */
 int main(void)
 {
-    char *s = "hello, world, bite";
-    char *f = "world, bite";
-    char *t;
+    int n;
 
-    t = _strstr(s, f);
-    printf("%s\n", t);
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
     return (0);
 }
