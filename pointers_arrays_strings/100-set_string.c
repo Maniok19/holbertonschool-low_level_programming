d29478cb6e3077828a8dc2484a19dbd57428ee0d
Mano <mano19029@hotmail.fr>
2024-11-06 10:58:14 +0100

    Add set_string function to main.h and implement it in 100-set_string.c

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_string - Sets the value of a pointer to a char
+ * @s: Pointer to a pointer to char
+ * @to: Pointer to char
+ *
+ * Description: This function sets the value of the pointer *s to the value
+ * of the pointer to.
+ */
+void set_string(char **s, char *to)
+{
+    *s = to;
+}
