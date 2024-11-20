c84754b6d01f1fde90253b56609e33dde0167100
Mano <mano19029@hotmail.fr>
2024-11-20 12:17:15 +0100

    Refactor hello world assembly code for clarity and organization

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,17 +1,17 @@
 global _start
 
+section .data
+    message db "Hello, World", 10  ; 10 is the ASCII code for newline
+    message_length equ $ - message
+
 section .text
 _start:
-	; write syscall
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, message
-	mov rdx, message_length
-	syscall
+    mov rax, 1          ; system call number for write
+    mov rdi, 1          ; file descriptor 1 is stdout
+    mov rsi, message    ; address of the message
+    mov rdx, message_length ; length of the message
+    syscall
 
     mov rax, 60         ; system call number for exit
     xor rdi, rdi        ; exit status 0
-	syscall
-section .data
-	message: db "Hello, World", 10
-	message_length equ $ - message
+    syscall
