b400ba9bf2d171cb0ad1f48c3f3b1be879156148
Mano <mano19029@hotmail.fr>
2024-11-20 12:49:54 +0100

    Format hello world assembly code for consistency and readability

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,14 +1,17 @@
 section .data
-    message db "Hello, World", 10
+
+	message db "Hello, World", 10
+
 section .text
+
 global main
 main:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, message
-    mov rdx, 13
-    syscall
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 13
+	syscall
 
-    mov rax, 60
-    mov rdi, 0
-    syscall
+	mov rax, 60
+	mov rdi, 0
+	syscall
