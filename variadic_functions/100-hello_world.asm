214fe65d5cf036a7c69519c66f4bdd697b68ad1e
Mano <mano19029@hotmail.fr>
2024-11-20 12:10:49 +0100

    fix length in rdx

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -6,7 +6,12 @@ _start:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, message
-	mov rdx, 13
+	mov rdx, message_length
+	syscall
+
+    mov rax, 60         ; system call number for exit
+    xor rdi, rdi        ; exit status 0
 	syscall
 section .data
 	message: db "Hello, World", 10
+	message_length equ $ - message
