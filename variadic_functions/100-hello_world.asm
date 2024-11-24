26cb2f8b3ad33774dbbbf637da2855c11845f698
Maniok <40887791+Maniok19@users.noreply.github.com>
2024-11-24 11:39:44 +0100

    Update 100-hello_world.asm

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,20 +1,17 @@
-section   .text
-
-global    _start
-
-	
-_start:
-	mov  rax, 1                  ; write(
-	mov  rdi, 1                  ;   STDOUT_FILENO,
-	mov  rsi, message            ;   "Hello, world!\n",
-	mov  rdx, 13                 ;   sizeof("Hello, world!\n")
-	syscall                      ; invoke operating system to do the write
-	mov  rax, 60                 ; exit(
-	xor  rdi, rdi                ; EXIT_SUCCESS
-	syscall                      ; invoke to exit
-
-	section   .data
-message:
-
-	db    "Hello, World", 10      ; note the newline at the end
-    msglen: equ $ - message           ; length of string
+ section .text
+ global main
+ 
+ main:
+     mov rax, 1
+     mov rdi, 1
+     mov rsi, msg
+     mov rdx, msglen
+     syscall
+ 
+     mov rax, 60
+     mov rdi, 0
+     syscall
+ 
+ section .rodata
+     msg: db "Hello, World", 10
+     msglen: equ $ - msg
