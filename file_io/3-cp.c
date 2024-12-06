1498aa2564fdb610e7c02963d2ce2b2f4c5ff142
Maniok19 <mano19029@hotmail.fr>
2024-12-06 15:29:28 +0100

    Add file copy functionality with error handling and buffer management

---
+++ file_io/3-cp.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+/**
+ * error_exit - prints an error message and exits with a status code
+ * @code: the status code to exit with
+ * @message: the error message to print
+ * @filename: the filename to print
+ * Return: void
+ */
+void error_exit(int code, const char *message, const char *filename)
+{
+	dprintf(STDERR_FILENO, message, filename);
+	exit(code);
+}
+/**
+ * main - copies the content of a file to another file
+ * @argc: the number of arguments
+ * @argv: the arguments
+ * Return: 0 on success, 97 if argc is not 3, 98 if file_from can't be read,
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, rd, wr;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+		error_exit(97, "Usage: cp file_from file_to\n", "");
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		{
+			close(fd_from);
+			error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+		}
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+	while ((rd = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		wr = write(fd_to, buffer, rd);
+		if (wr != rd)
+		{
+			close(fd_from);
+			close(fd_to);
+			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+		}
+	}
+	if (rd == -1)
+	{
+		close(fd_from);
+		close(fd_to);
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+	}
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
