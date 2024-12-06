f401b2fa4261db65640815dae9c561125fd65d10
Maniok19 <mano19029@hotmail.fr>
2024-12-06 15:33:11 +0100

    Refactor error handling in file copy function for improved readability and efficiency

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -29,41 +29,30 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		error_exit(97, "Usage: cp file_from file_to\n", "");
+
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		{
-			close(fd_from);
-			error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-		}
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+
 	while ((rd = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		wr = write(fd_to, buffer, rd);
-		if (wr != rd)
-		{
-			close(fd_from);
-			close(fd_to);
+		if (wr != rd || wr == -1)
 			error_exit(99, "Error: Can't write to %s\n", argv[2]);
-		}
 	}
+
 	if (rd == -1)
-	{
-		close(fd_from);
-		close(fd_to);
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-	}
+
 	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
+		error_exit(100, "Error: Can't close fd %d\n", fd_from);
+
 	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+		error_exit(100, "Error: Can't close fd %d\n", fd_to);
 
 	return (0);
 }
